b028be5fb56b299cbe033dd206075ad6c4c65fa2
Namju Kim <cmsong111@naver.com>
2023-02-09 04:16:14 +0900

    initial commit
    make project with https://start.spring.io/
    
    gradle - groovy
    java 11
    Springboot 2.7.8
    
    with Dependencies
    spring-boot-starter-data-jpa
    spring-boot-starter-oauth2-client
    spring-boot-starter-security
    spring-boot-starter-web
    spring-cloud-gcp-starter
    springdoc-openapi-ui:1.6.14
    mariadb-java-client', version: '3.1.0
    com.h2database:h2
    lombok

---
+++ src/main/java/com/ohh_really/ringdingdong/RingdingdongApplication.java
@@ -0,0 +1,14 @@
+package com.ohh_really.ringdingdong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RingdingdongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RingdingdongApplication.class, args);
+	}
+
+}
+
