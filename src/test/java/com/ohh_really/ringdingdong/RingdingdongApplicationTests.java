2491700b5bf95b587fca346ccdff66b0f529f83b
Namju Kim <cmsong111@naver.com>
2023-02-09 04:16:14 +0900

    initial commit
    make project with https://start.spring.io/
    
    gradle - groovy
    java 11
    Springboot 2.7.8
    
    with Dependencies
    spring-boot-starter-data-jpa
    spring-boot-starter-oauth2-client
    spring-boot-starter-security
    spring-boot-starter-web
    spring-cloud-gcp-starter
    springdoc-openapi-ui:1.6.14
    mariadb-java-client', version: '3.1.0
    com.h2database:h2
    lombok

---
+++ src/test/java/com/ohh_really/ringdingdong/RingdingdongApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ohh_really.ringdingdong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class RingdingdongApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
