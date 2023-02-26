a40bac4c5c2ef7939ad8db7467e079255e74ca7e
Namju Kim <cmsong111@naver.com>
2023-02-27 02:05:33 +0900

    feat: 지역 검색 기능 추가

--- src/main/java/com/ohh_really/ringdingdong/location/controller/LocationController.java
+++ src/main/java/com/ohh_really/ringdingdong/location/controller/LocationController.java
@@ -1,14 +1,35 @@
 package com.ohh_really.ringdingdong.location.controller;
 
+import com.ohh_really.ringdingdong.location.entity.Location;
 import com.ohh_really.ringdingdong.location.service.LocationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
 @RestController
+@RequestMapping("/location")
+@Tag(name = "Location", description = "Location API")
 public class LocationController {
     private final LocationService locationService;
     @Autowired
     public LocationController(LocationService locationService) {
         this.locationService = locationService;
+    }
+
+    @GetMapping("/find")
+    @Operation(summary = "Find location by country, state and city")
+    public ResponseEntity<ArrayList<Location>> find(
+            @RequestParam String country,
+            @RequestParam String state,
+            @RequestParam String city
+    ) {
+        return locationService.findByCountryAndStateAndCity(country, state, city);
     }
 }
--- src/main/java/com/ohh_really/ringdingdong/location/service/LocationService.java
+++ src/main/java/com/ohh_really/ringdingdong/location/service/LocationService.java
@@ -1,8 +1,13 @@
 package com.ohh_really.ringdingdong.location.service;
 
+import com.ohh_really.ringdingdong.location.entity.Location;
 import com.ohh_really.ringdingdong.location.repository.LocationRepository;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 public class LocationService {
@@ -11,6 +16,10 @@ public class LocationService {
     @Autowired
     public LocationService(LocationRepository locationRepository) {
         this.locationRepository = locationRepository;
+    }
+
+    public ResponseEntity<ArrayList<Location>> findByCountryAndStateAndCity(String country, String state, String city) {
+        return ResponseEntity.ok(locationRepository.findByCountryAndStateAndCity(country, state, city));
     }
 
 }
